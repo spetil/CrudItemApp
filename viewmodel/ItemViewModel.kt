2659aacea0d7eec74797470a4b5ecfbd6917c511
spetil <109627646+spetil@users.noreply.github.com>
2025-07-03 13:54:03 -0300

    Add files via upload

---
+++ viewmodel/ItemViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.cruditemapp.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.cruditemapp.model.Item
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import kotlin.math.log
+
+
+class ItemViewModel: ViewModel(
+) {
+    private val db = FirebaseFirestore.getInstance()
+    private var listenerRegistration: ListenerRegistration ? = null
+
+    var items = mutableStateOf<List<Item>>(listOf())
+        private set
+    init {
+
+    }
+
+    private fun listenerToItems(){
+        listenerRegistration = db.collection("items")
+            .addSnapshotListener{snapshot, exception ->
+                if (exception != null){
+                    return@addSnapshotListener
+                }
+                if (snapshot != null){
+                    val fetchedItems = snapshot.documents.map { document ->
+                        document.toObject(Item::class.java)?.copy(id = document.id)
+                    }.filterNotNull()
+                    items.value = fetchedItems
+                }
+            }
+    }
+
+    fun addItem(item: Item){
+        db.collection("items").add(item)
+    }
+
+    fun deleteItem(itemId: String){
+        db.collection("items").document(itemId).delete()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        listenerRegistration?.remove()
+    }
+
+    fun updateItem(item: Item){
+        db.collection("items")
+            .document(item.id)
+            .set(item)
+            .addOnSuccessListener {
+                Log.d("viewmodel","Item atualizado com sucesso")
+            }
+            .addOnSuccessListener {
+                Log.d("viewmodel","Item não atualizado com sucesso")
+            }
+    }
+
+
+}
